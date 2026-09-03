a7644f11178819a2bd44264e8c42a3597c64c8fa
harshit12505 <146528487+harshit12505@users.noreply.github.com>
2026-09-03 23:18:57 +0530

    Add: valid-word-abbreviation - submission-37

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-37.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int i = 0;
+        int j = 0;
+
+        while(i < word.length() && j< abbr.length()){
+            char a = word.charAt(i);
+            char b = abbr.charAt(j);
+
+            if(Character.isLetter(b)){
+                if(a!= b){
+                    return false;
+                }
+                else{
+                    i++;
+                    j++;
+                }
+            }
+            else{
+                if(b == '0'){
+                    return false;
+                }
+                int current = 0;
+                while(j< abbr.length() && Character.isDigit(abbr.charAt(j))){
+                    current = current*10 + (abbr.charAt(j)-'0');
+                    j++;
+                }
+                i += current;
+            }
+        }
+        return i == word.length() && j == abbr.length();
+    }
+}
